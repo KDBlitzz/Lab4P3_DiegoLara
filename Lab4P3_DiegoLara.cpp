866ed8b9b4d500525ade79ccaf43cf166ed578d6
Diego Lara <diegoalara2005@gmail.com>
2024-02-09 17:33:34 -0600

    SexTO Commit

--- Lab4P3_DiegoLara.cpp
+++ Lab4P3_DiegoLara.cpp
@@ -11,14 +11,14 @@ int generarNumRandom(int minimo, int maximo) { // metodo para generar numeros al
 	random = minimo + (rand() % maximo);
 	return random;
 }
-void reservarMatrizEntero(int**& matriz, int tamano) {
+void reservarMatrizEntero(int**& matriz, int tamano) { // Reservar la memoria de la matriz
 	matriz = new int* [tamano];
 	for (int i = 0; i < tamano; i++)
 	{
 		matriz[i] = new int[tamano];
 	}
 }
-void llenarMatrizEnteros(int**& matriz, int tamano) {
+void llenarMatrizEnteros(int**& matriz, int tamano) { // Llenar la matriz de entreos
 	reservarMatrizEntero(matriz, tamano);
 	for (int i = 0; i < tamano; i++)
 	{
@@ -37,7 +37,7 @@ void llenarMatrizEnteros(int**& matriz, int tamano) {
 		cout << " ]" << endl;
 	}
 }
-int calcularInversaMatematica(int**& matriz, int tamano) {
+int calcularInversaMatematica(int**& matriz, int tamano) { // Calcular las diagonales
 	int sumaDiagonal = 0;
 	for (int i = 0; i < tamano; i++)
 	{
@@ -55,7 +55,7 @@ int calcularInversaMatematica(int**& matriz, int tamano) {
 	}
 	return sumaDiagonal;
 }
-void ejercicio_1() {
+void ejercicio_1() { // Ejercicio 1
 	int** matriz = nullptr;
 	int tamano = 0;
 	int totalSumaDiagonal = 0;
@@ -76,14 +76,14 @@ void ejercicio_1() {
 	}
 	delete matriz;
 }
-void reservarMatriz(char**& matriz, int filas, int columnas) {
+void reservarMatriz(char**& matriz, int filas, int columnas) { // Reservar la memoria de la matriz
 	matriz = new char* [filas];
 	for (int i = 0; i < filas; i++)
 	{
 		matriz[i] = new char[columnas];
 	}
 }
-void llenarMatriz(char**& matriz, int filas, int columnas) {
+void llenarMatriz(char**& matriz, int filas, int columnas) { // Llenar la matriz con caracteres
 	reservarMatriz(matriz, filas, columnas);
 	for (int i = 0; i < filas; i++)
 	{
@@ -93,7 +93,7 @@ void llenarMatriz(char**& matriz, int filas, int columnas) {
 		}
 	}
 }
-void imprimirMatriz(char**& matriz, int filas, int columnas) {
+void imprimirMatriz(char**& matriz, int filas, int columnas) { // Imprimir la matriz
 	llenarMatriz(matriz, filas, columnas);
 	for (int i = 0; i < filas; i++)
 	{
@@ -105,8 +105,8 @@ void imprimirMatriz(char**& matriz, int filas, int columnas) {
 		cout << " ]" << endl;
 	}
 }
-void buscarPalabra(char**& matriz, int filas, int columnas, string palabraBuscada) {
-	bool fraseEncontrada = false;
+void buscarPalabra(char**& matriz, int filas, int columnas, string palabraBuscada) { // buscar la palabra adentro de la matriz
+	bool encontrado = false;
 	for (int i = 0; i < filas; i++)
 	{
 		string temporal = "";
@@ -114,16 +114,33 @@ void buscarPalabra(char**& matriz, int filas, int columnas, string palabraBuscad
 		{
 			temporal += matriz[i][j];
 		}
+		if (palabraBuscada.compare(temporal) == 0)
+		{
+			cout << "La palabra " << palabraBuscada << " SI se encuentra en la sopa de letras" << endl;
+			encontrado = true;
+			break;
+		}
 	}
-	if (fraseEncontrada)
+	for (int i = 0; i < filas; i++)
 	{
-		cout << "La palabra " << palabraBuscada << " SI se encuentra en la sopa de letras" << endl;
+		string temporal = "";
+		for (int j = 0; j < columnas; j++)
+		{
+			temporal += matriz[j][i];
+		}
+		if (palabraBuscada.compare(temporal) == 0)
+		{
+			cout << "La palabra " << palabraBuscada << " SI se encuentra en la sopa de letras" << endl;
+			encontrado = true;
+			break;
+		}
 	}
-	else {
+	if (!encontrado)
+	{
 		cout << "La palabra " << palabraBuscada << " NO se encuentra en la sopa de letras" << endl;
 	}
 }
-void ejercicio_2() {
+void ejercicio_2() { // Ejercicio 2
 	int fila = 0;
 	int columna = 0;
 	string palabraBuscada = "";
@@ -145,11 +162,16 @@ void ejercicio_2() {
 	cout << "Ingrese la palabra a buscar en la sopa de letras: "; cin >> palabraBuscada;
 	cout << endl;
 	
-	
 	buscarPalabra(matriz, fila, columna, palabraBuscada);
 	cout << endl;
+
+	for (int i = 0; i < columna; i++)
+	{
+		delete[] matriz[i];
+	}
+	delete matriz;
 }
-void menu() {
+void menu() { // Menu
 	bool menu = true;
 	int option = 0;
 	while (menu) {
